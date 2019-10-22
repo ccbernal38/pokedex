a83443103742683e9e26f8392ba73adef75ffa8e
Christian Camilo Bernal <bernal.9219@gmail.com>
2019-10-22 17:35:45 -0500

    Listado de pokemon con imagen y tipos

--- pokedex/controllers/PokemonViewController.swift
+++ pokedex/controllers/PokemonViewController.swift
@@ -9,10 +9,26 @@
 import UIKit
 import Alamofire
 
-class PokemonViewController: UIViewController {
-   
+class PokemonViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
     @IBOutlet weak var tableview: UITableView!
-
+    var pokeArray: [[String:Any]] = []
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pokeArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "tableViewCellPokemon", for: indexPath) as! PokemonTableViewCell
+        let row = self.pokeArray[indexPath.row]
+        cell.nombre.text = row["name"] as? String ?? ""
+        cell.url = row["url"] as? String ?? ""
+        cell.cargarInfo()
+        return cell
+    }
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -20,13 +36,23 @@ class PokemonViewController: UIViewController {
     }
     
     func listadoPokemon() {
-        AF.request(Utils.urlPokemonInit)
+        AF.request(Utils.urlPokemonInit,headers: [
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        ])
             .validate(statusCode: 200..<600)
             .responseJSON { response in
                 switch response.result
                 {
                     case .success:
-                        print(response.result)
+                        if let json = response.value as? NSDictionary {
+                            if let result = json["results"] as? [[String:Any]] {
+                                for pokemon in result {
+                                    self.pokeArray.append(pokemon)
+                                }
+                                self.tableview.reloadData()
+                            }
+                        }
                     case .failure(let error):
                         print(error)
                 }
